9237169dff2fc61911da45ad8eea0aafba716fef
Robert McGibbon <rmcgibbo@gmail.com>
2015-02-08 23:27:56 -0800

    SGE working

--- main.go
+++ main.go
@@ -1,124 +1,45 @@
+// -*-  tab-width:4  -*-
 package main
 
 import (
-	// "free_slots"
-	"encoding/xml"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"github.com/BurntSushi/ty/fun"
 	"os"
-	"strconv"
-	"strings"
-	// "os/exec"
+	"sge"
+	"text/tabwriter"
 )
 
-type Status struct {
-	Queue    string
-	np_alloc int
-	np_total int
-}
-
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("Summary of SGE nodes with free slots\n\n")
 
-	counts := Collect()
-	fmt.Println(counts)
-	ParseQconfMap()
-}
-func ParseQconfList() []string {
-}
+	counts := sge.CollectFreeSlots()
 
-func ParseQconfMap() map[string]string {
-	file, err := os.Open("qconf.sq.batch.q.xml")
-	if err != nil {
-		log.Fatal(err)
+	rows := []string{
+		"Number of Nodes\tQueue\tUtilization\tFree slots",
+		"---------------\t-----\t-----------\t----------"}
+
+	keys := make([]sge.NodeStatus, 0, len(counts))
+	for k := range counts {
+		keys = append(keys, k)
 	}
-	defer file.Close()
-	inBytes, _ := ioutil.ReadAll(file)
-	lines := strings.Split(string(inBytes), "\n")
-
-	var withinMultilineEntry = false
-	var key = ""
-	var value = ""
-
-	d := make(map[string]string)
-
-	for _, line := range lines {
-		if withinMultilineEntry {
-			value += strings.TrimSpace(strings.TrimSuffix(line, "\\"))
-			withinMultilineEntry = strings.HasSuffix(line, "\\")
-
-		} else {
-			fields := strings.SplitN(strings.TrimSpace(line), " ", 2)
-			if len(fields) == 2 {
-				key = strings.TrimSpace(fields[0])
-				withinMultilineEntry = strings.HasSuffix(line, "\\")
-				value = strings.TrimSpace(strings.TrimSuffix(fields[1], "\\"))
-
-			}
+	fun.Sort(func(a, b sge.NodeStatus) bool {
+		if a.Queue == b.Queue {
+			return (a.NpTotal - a.NpAlloc) > (b.NpTotal - b.NpAlloc)
 		}
-
-		if !withinMultilineEntry {
-			d[key] = value
-		}
-
+		return a.Queue < b.Queue
+	}, keys)
+	for _, k := range keys {
+		rows = append(rows, fmt.Sprintf("%d\t%s\t%d/%d\t%d",
+			counts[k], k.Queue, k.NpAlloc, k.NpTotal, k.NpTotal-k.NpAlloc))
 	}
-	fmt.Println(d)
-	return d
+	PrintTable(rows)
 }
 
-func QueuePermissions() {
-    queues = parse_qconf('-sql')
-
-}
-
-func Collect() map[Status]int {
-	type Item struct {
-		Data  string `xml:",chardata"`
-		Name  string `xml:"name,attr"`
-		QName string `xml:"qname,attr"`
+func PrintTable(rows []string) {
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	for _, line := range rows {
+		fmt.Fprintln(w, line)
 	}
-	type Host struct {
-		Name       string `xml:"name,attr"`
-		Hostvalues []Item `xml:"hostvalue"`
-		Queuevalue []Item `xml:"queue>queuevalue"`
-	}
-	type QHost struct {
-		XMLName xml.Name `xml:"qhost"`
-		Hosts   []Host   `xml:"host"`
-	}
-
-	file, err := os.Open("qhost_q_xml.xml")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	xmlData, _ := ioutil.ReadAll(file)
-
-	var q QHost
-	xml.Unmarshal(xmlData, &q)
-
-	counts := make(map[Status]int)
-
-	for _, h := range q.Hosts {
-		var slots_used = -1
-		var slots = -1
-		for _, v := range h.Queuevalue {
-			if v.Name == "slots_used" {
-				slots_used, _ = strconv.Atoi(v.Data)
-			} else if v.Name == "slots" {
-				slots, _ = strconv.Atoi(v.Data)
-			}
-		}
-		if slots_used != -1 && slots != -1 {
-			s := Status{h.Queuevalue[0].QName, slots_used, slots}
-			_, ok := counts[s]
-			if ok {
-				counts[s]++
-			} else {
-				counts[s] = 1
-			}
-		}
-	}
-	return counts
+	w.Flush()
 }
