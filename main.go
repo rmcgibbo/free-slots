10ae3842b62d929190851c78ef065b03db1ddad2
Robert McGibbon <rmcgibbo@gmail.com>
2015-02-09 01:00:02 -0800

    Add torque

--- main.go
+++ main.go
@@ -7,11 +7,41 @@ import (
 	"os"
 	"sge"
 	"text/tabwriter"
+	"torque"
 )
 
 func main() {
-	fmt.Printf("Summary of SGE nodes with free slots\n\n")
+	if torque.IsTorque() {
+		mainTORQUE()
+	}
+}
 
+func mainTORQUE() {
+	fmt.Printf("Summary of PBS nodes with free slots\n\n")
+	counts := torque.CollectFreeSlots()
+	var _ = counts
+	rows := []string{
+		"Number of Nodes  \tProperties\tUtilization\tFree slots",
+		"---------------  \t----------\t-----------\t----------"}
+
+	keys := make([]torque.NodeStatus, 0, len(counts))
+	for k := range counts {
+		keys = append(keys, k)
+	}
+	fun.Sort(func(a, b torque.NodeStatus) bool {
+		return float64(a.NpAlloc)/float64(a.NpTotal) <
+			float64(b.NpAlloc)/float64(b.NpTotal)
+	}, keys)
+	for _, k := range keys {
+		rows = append(rows, fmt.Sprintf("%d\t%s\t%d/%d\t%d",
+			counts[k], k.Properties, k.NpAlloc, k.NpTotal, k.NpTotal-k.NpAlloc))
+	}
+	printTable(rows)
+
+}
+
+func mainSGE() {
+	fmt.Printf("Summary of SGE nodes with free slots\n\n")
 	counts := sge.CollectFreeSlots()
 
 	rows := []string{
@@ -32,10 +62,10 @@ func main() {
 		rows = append(rows, fmt.Sprintf("%d\t%s\t%d/%d\t%d",
 			counts[k], k.Queue, k.NpAlloc, k.NpTotal, k.NpTotal-k.NpAlloc))
 	}
-	PrintTable(rows)
+	printTable(rows)
 }
 
-func PrintTable(rows []string) {
+func printTable(rows []string) {
 	w := new(tabwriter.Writer)
 	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
 	for _, line := range rows {
--- src/torque/torque.go
+++ src/torque/torque.go
@@ -2,5 +2,64 @@
 package torque
 
 import (
-    "os/exec"
+	"encoding/xml"
+	"log"
+	"os/exec"
+	"strconv"
+	"strings"
 )
+
+// Does this system use Torque?
+func IsTorque() bool {
+	_, err := exec.LookPath("pbsnodes")
+	return err == nil
+}
+
+type NodeStatus struct {
+	Properties string
+	NpAlloc    int
+	NpTotal    int
+}
+
+func CollectFreeSlots() map[NodeStatus]int {
+	type Node struct {
+		State      string `xml:"state"`
+		Properties string `xml:"properties"`
+		NpTotal    string `xml:"np"`
+		Jobs       string `xml:"jobs"`
+	}
+	type Data struct {
+		XMLName xml.Name `xml:"Data"`
+		Nodes   []Node   `xml:"Node"`
+	}
+
+	xmlData, err := exec.Command("pbsnodes", "-x").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	var root Data
+	xml.Unmarshal(xmlData, &root)
+	counts := make(map[NodeStatus]int)
+
+	for _, n := range root.Nodes {
+		if n.State != "free" {
+			continue
+		}
+
+		var NpAlloc = 0
+		if n.Jobs != "" {
+			NpAlloc = len(strings.Split(n.Jobs, ", "))
+		}
+		NpTotal, _ := strconv.Atoi(n.NpTotal)
+
+		ns := NodeStatus{n.Properties, NpAlloc, NpTotal}
+		_, ok := counts[ns]
+		if ok {
+			counts[ns]++
+		} else {
+			counts[ns] = 1
+		}
+	}
+
+	return counts
+}
